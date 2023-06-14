2973e931f63dcd213b4839cf26d8e3e177bcde06
Amanda Mendes <amandamendes@Amandas-MacBook-Air.local>
2023-06-14 03:01:16 -0300

    subindo exercicios Oracle 1 ao 8

---
+++ src/ex1.java
@@ -0,0 +1,13 @@
+package src;
+
+public class Main {
+    public static void main(String[] args) {
+        //print padrao de hello world
+        System.out.print("Hello World!");
+        //print aleatorio
+        System.out.print("Olá!");
+
+        //ava faz distinção entre letras maiúsculas e minúsculas
+        //
+    }
+}
